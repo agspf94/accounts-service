da08f3fc47909ca2c0bf3d65cfb5487f273f72c8
Anderson Fantin <agspf94@gmail.com>
2022-09-01 23:10:04 -0300

    [agspf94] Added Spring Web

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,15 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    // Default
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Spring Web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // Testing - Default
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
