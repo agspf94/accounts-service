2028cdac328fe01fbd59f9cf2f22d3a7e5dadbf1
Anderson Fantin <agspf94@gmail.com>
2022-09-02 00:06:17 -0300

    [agspf94] Added Home Service to Home Controller

--- src/main/kotlin/com/example/accountsservice/controller/HomeController.kt
+++ src/main/kotlin/com/example/accountsservice/controller/HomeController.kt
@@ -1,14 +1,17 @@
 package com.example.accountsservice.controller
 
+import com.example.accountsservice.service.HomeService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/")
-class HomeController {
+class HomeController(
+    private val homeService: HomeService
+) {
     @GetMapping
-    fun getHome(): String {
-        return "DevPath: accounts-service"
+    fun welcome(): String {
+        return homeService.welcome()
     }
 }
