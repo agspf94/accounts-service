35c1c38d182057a1ae36452fd2c5ec889899c527
Anderson Fantin <agspf94@gmail.com>
2022-09-02 00:06:56 -0300

    [agspf94] Added comment on AccountsServiceApplicationTests

--- src/test/kotlin/com/example/accountsservice/AccountsServiceApplicationTests.kt
+++ src/test/kotlin/com/example/accountsservice/AccountsServiceApplicationTests.kt
@@ -3,10 +3,13 @@ package com.example.accountsservice
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+/*
+Make sure that the application context can start
+ */
+
 @SpringBootTest
 class AccountsServiceApplicationTests {
     @Test
     fun contextLoads() {
-
     }
 }
