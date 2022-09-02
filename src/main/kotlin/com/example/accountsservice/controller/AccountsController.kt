356bcdea1d85a4902c4b06c9590eca20c1bec038
Anderson Fantin <agspf94@gmail.com>
2022-09-02 00:18:05 -0300

    [agspf94] Added AccountsController, Service and Repository

---
+++ src/main/kotlin/com/example/accountsservice/controller/AccountsController.kt
@@ -0,0 +1,8 @@
+package com.example.accountsservice.controller
+
+import com.example.accountsservice.service.AccountsService
+
+class AccountsController(
+    private val accountsService: AccountsService
+) {
+}
---
+++ src/main/kotlin/com/example/accountsservice/service/AccountsService.kt
@@ -0,0 +1,8 @@
+package com.example.accountsservice.service
+
+import com.example.accountsservice.repository.AccountsRepository
+
+class AccountsService(
+    private val accountsRepository: AccountsRepository
+) {
+}
