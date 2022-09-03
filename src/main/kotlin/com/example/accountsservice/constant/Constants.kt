e654f509af7cc140310d915928d2963e8bcf5b2a
Anderson Fantin <agspf94@gmail.com>
2022-09-03 18:04:34 -0300

    [agspf94] Added new constants

--- src/main/kotlin/com/example/accountsservice/constant/Constants.kt
+++ src/main/kotlin/com/example/accountsservice/constant/Constants.kt
@@ -3,5 +3,8 @@ package com.example.accountsservice.constant
 class Constants {
     companion object {
         const val WELCOME = "Welcome to DevPath. This is the accounts-service"
+        const val NOT_FOUND = "Account id not found"
+        const val DELETE_MESSAGE = "Account deleted successfully"
+        const val EMPTY_LIST = "No accounts have been created yet"
     }
 }
