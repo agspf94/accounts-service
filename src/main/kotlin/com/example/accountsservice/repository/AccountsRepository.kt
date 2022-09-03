b69bf2145f1a9326e6e6c435a84d44b9d0fba023
Anderson Fantin <agspf94@gmail.com>
2022-09-03 18:04:12 -0300

    [agspf94] Change AccountsRepository from CrudRepository to JpaRepository

--- src/main/kotlin/com/example/accountsservice/repository/AccountsRepository.kt
+++ src/main/kotlin/com/example/accountsservice/repository/AccountsRepository.kt
@@ -1,8 +1,8 @@
 package com.example.accountsservice.repository
 
 import com.example.accountsservice.entity.Account
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AccountsRepository : CrudRepository<Account, Long>
+interface AccountsRepository : JpaRepository<Account, Long>
