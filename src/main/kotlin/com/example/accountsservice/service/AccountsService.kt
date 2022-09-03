e660ddcaaeb226cd2e3be85b5cab2fb1c7865443
Anderson Fantin <agspf94@gmail.com>
2022-09-03 18:04:27 -0300

    [agspf94] Added getAccount, getAllAccounts, editAccount and deleteAccount

--- src/main/kotlin/com/example/accountsservice/service/AccountsService.kt
+++ src/main/kotlin/com/example/accountsservice/service/AccountsService.kt
@@ -1,15 +1,58 @@
 package com.example.accountsservice.service
 
+import com.example.accountsservice.constant.Constants.Companion.DELETE_MESSAGE
+import com.example.accountsservice.constant.Constants.Companion.EMPTY_LIST
+import com.example.accountsservice.constant.Constants.Companion.NOT_FOUND
 import com.example.accountsservice.dto.request.AccountRequest
+import com.example.accountsservice.dto.response.DeleteResponse
 import com.example.accountsservice.entity.Account
+import com.example.accountsservice.exception.EmptyListException
 import com.example.accountsservice.repository.AccountsRepository
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import java.util.stream.Collectors
 
 @Service
 class AccountsService(
     private val accountsRepository: AccountsRepository
 ) {
-    fun create(accountRequest: AccountRequest): Account {
+    fun createAccount(accountRequest: AccountRequest): Account {
         return accountsRepository.save(accountRequest.toAccount())
+    }
+
+    fun getAccount(id: Long): Account {
+        return accountsRepository.findById(id)
+            .map { it }
+            .orElseThrow { NoSuchElementException(NOT_FOUND) }
+    }
+
+    fun getAllAccounts(pageable: Pageable): List<Account> {
+        return accountsRepository.findAll(pageable)
+            .stream()
+            .collect(Collectors.toList())
+            .ifEmpty { throw EmptyListException(EMPTY_LIST) }
+    }
+
+    fun editAccount(account: Account): Account? {
+        return accountsRepository.findById(account.id)
+            .map {
+                accountsRepository.save(
+                    Account(
+                        account.id,
+                        account.name ?: it.name,
+                        account.birthday ?: it.birthday
+                    )
+                )
+            }
+            .orElseThrow { NoSuchElementException(NOT_FOUND) }
+    }
+
+    fun deleteAccount(id: Long): DeleteResponse? {
+        return accountsRepository.findById(id)
+            .map {
+                accountsRepository.deleteById(id)
+                DeleteResponse(id, DELETE_MESSAGE)
+            }
+            .orElseThrow { NoSuchElementException(NOT_FOUND) }
     }
 }
