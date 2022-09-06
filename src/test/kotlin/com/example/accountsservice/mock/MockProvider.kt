5b414aa44da8f4742838a2d45ace630c9d4fc478
Anderson Fantin <agspf94@gmail.com>
2022-09-06 14:09:07 -0300

    [agspf94] Added new tests to AccountsServiceTest

--- src/test/kotlin/com/example/accountsservice/mock/MockProvider.kt
+++ src/test/kotlin/com/example/accountsservice/mock/MockProvider.kt
@@ -2,6 +2,8 @@ package com.example.accountsservice.mock
 
 import com.example.accountsservice.dto.request.AccountRequest
 import com.example.accountsservice.entity.Account
+import org.springframework.data.domain.PageRequest
+import kotlin.random.Random
 
 class MockProvider {
     companion object {
@@ -10,7 +12,19 @@ class MockProvider {
         }
 
         fun getAccount(): Account {
-            return Account(1, "Anderson", "1994-01-13")
+            return Account(Random.nextLong(), "Anderson", "1994-01-13")
+        }
+
+        fun getPageable(): PageRequest {
+            return PageRequest.of(0, 2)
+        }
+
+        fun getNewAccount(oldAccount: Account): Account {
+            return Account(
+                oldAccount.id,
+                oldAccount.name + " - EDITED NAME",
+                oldAccount.birthday + " - EDITED BIRTHDAY"
+            )
         }
     }
 }
--- src/test/kotlin/com/example/accountsservice/service/accounts/AccountsServiceTest.kt
+++ src/test/kotlin/com/example/accountsservice/service/accounts/AccountsServiceTest.kt
@@ -1,18 +1,26 @@
 package com.example.accountsservice.service.accounts
 
+import com.example.accountsservice.constant.Constants.Companion.DELETE_MESSAGE
 import com.example.accountsservice.entity.Account
+import com.example.accountsservice.exception.EmptyListException
 import com.example.accountsservice.mock.MockProvider.Companion.getAccount
 import com.example.accountsservice.mock.MockProvider.Companion.getAccountRequest
+import com.example.accountsservice.mock.MockProvider.Companion.getNewAccount
+import com.example.accountsservice.mock.MockProvider.Companion.getPageable
 import com.example.accountsservice.repository.AccountsRepository
 import com.example.accountsservice.service.AccountsService
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito.doNothing
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.data.domain.PageImpl
 import java.util.Optional
+
 
 @SpringBootTest
 class AccountsServiceTest {
@@ -51,5 +59,86 @@ class AccountsServiceTest {
         val accountReturned = accountsService.getAccount(id)
 
         assertEquals(account.id, accountReturned.id)
+    }
+
+    @Test
+    fun `getAccount should throw no such element exception when the account does not exist`() {
+        val id = 1L
+
+        `when`(accountsRepository.findById(id)).thenReturn(Optional.empty())
+
+        assertThrows<NoSuchElementException> { accountsService.getAccount(id) }
+    }
+
+    @Test
+    fun `getAllAccounts should return a list with all created accounts`() {
+        val pageable = getPageable()
+        val accountsList = listOf(getAccount(), getAccount())
+        val accountsListPage = PageImpl(accountsList, pageable, accountsList.size.toLong())
+
+        `when`(accountsRepository.findAll(pageable)).thenReturn(accountsListPage)
+
+        val returnedAccountsList = accountsService.getAllAccounts(pageable)
+
+        assertEquals(accountsList.size, returnedAccountsList.size)
+        assertEquals(accountsList.first(), returnedAccountsList.first())
+        assertEquals(accountsList.last(), returnedAccountsList.last())
+    }
+
+    @Test
+    fun `getAllAccounts should throw empty list exception when there are no accounts created yet`() {
+        val pageable = getPageable()
+        val accountsList = listOf<Account>()
+        val accountsListPage = PageImpl(accountsList, pageable, 0)
+
+        `when`(accountsRepository.findAll(pageable)).thenReturn(accountsListPage)
+
+        assertThrows<EmptyListException> { accountsService.getAllAccounts(pageable) }
+    }
+
+    @Test
+    fun `editAccount should edit an account successfully`() {
+        val oldAccount = getAccount()
+        val newAccount = getNewAccount(oldAccount)
+
+        `when`(accountsRepository.findById(newAccount.id)).thenReturn(Optional.of(oldAccount))
+        `when`(accountsRepository.save(newAccount)).thenReturn(newAccount)
+
+        val editedAccount = accountsService.editAccount(newAccount)
+
+        assertEquals(newAccount.id, editedAccount.id)
+        assertEquals(newAccount.name, editedAccount.name)
+        assertEquals(newAccount.birthday, editedAccount.birthday)
+    }
+
+    @Test
+    fun `editAccount should throw no such element exception when the account does not exist`() {
+        val account = getAccount()
+
+        `when`(accountsRepository.findById(account.id)).thenReturn(Optional.empty())
+
+        assertThrows<NoSuchElementException> { accountsService.editAccount(account) }
+    }
+
+    @Test
+    fun `deleteAccount should delete an account successfully`() {
+        val id = 1L
+
+        `when`(accountsRepository.findById(id)).thenReturn(Optional.of(getAccount()))
+        doNothing().`when`(accountsRepository).deleteById(id)
+
+        val response = accountsService.deleteAccount(id)
+
+        assertEquals(id, response.id)
+        assertEquals(DELETE_MESSAGE, response.message)
+    }
+
+    @Test
+    fun `deleteAccount should throw no such element exception when the account does not exist`() {
+        val id = 1L
+
+        `when`(accountsRepository.findById(id)).thenReturn(Optional.empty())
+
+        assertThrows<NoSuchElementException> { accountsService.deleteAccount(id) }
     }
 }
